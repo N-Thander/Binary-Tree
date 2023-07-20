51a79a3b276a749017208a246f345ac25c86ff20
N-Thander <108251321+N-Thander@users.noreply.github.com>
2023-07-20 18:08:51 +0530

    Create binaryTree.c

---
+++ binaryTree.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+    int info;
+    struct node *left, *right;
+};
+
+typedef struct node N;
+
+N* newNode(int data)
+{
+    N *ptr;
+    ptr = (N*)malloc(sizeof(N));
+
+    ptr -> info = data;
+    ptr -> left = NULL;
+    ptr -> right = NULL;
+
+    return ptr;
+}
+
+void preorderTraversal(N *rootNode)
+{
+    if (rootNode == NULL)
+    {
+        return;
+    }
+    printf("%d ", rootNode -> info);
+    
+    preorderTraversal(rootNode -> left);
+    preorderTraversal(rootNode -> right);
+
+}
+
+
+void postorderTraversal(N *rootNode)
+{
+    if (rootNode == NULL)
+    {
+        return;
+    }
+
+    postorderTraversal(rootNode -> left);
+    postorderTraversal(rootNode -> right);
+    printf("%d ", rootNode -> info);
+
+}
+
+void inorderTraveresal(N *rootNode)
+{
+    if (rootNode == NULL)
+    {
+        return;
+    }
+    inorderTraveresal(rootNode -> left);
+    printf("%d ", rootNode -> info);
+    inorderTraveresal(rootNode -> right);
+
+}
+
+int treeSize(N * rootNode)
+{
+    if (rootNode == NULL)
+    {
+        return 0;
+    }
+    return treeSize(rootNode->left) + treeSize(rootNode->right) + 1;
+}
